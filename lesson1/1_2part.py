f07e5f6af831086adf7368720906a7aadbafef70
maksim.serbichenko <mserbichenko@gmail.com>
2019-11-18 15:12:55 +0200

    4 commit

--- lesson1/1_2part.py
+++ lesson1/1_2part.py
@@ -1,15 +1,15 @@
 class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
     def print_info(self):
         print(self.name, "is", self.age)
 
 
-john = Person()
-john.name = "John"
-john.age = 22
+john = Person("John", 22)
 
-lucy: Person = Person()
-lucy.name = "Lucy"
-lucy.age = 20
+lucy = Person("Lucy", 20)
 
 john.print_info()
 lucy.print_info()
