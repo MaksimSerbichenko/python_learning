7c5259b8087e826fe88760a6cdd364a3e9dcc293
maksim.serbichenko <mserbichenko@gmail.com>
2019-12-04 14:40:45 +0200

    2 lesson 2_11part

---
+++ lesson2/2_11part.py
@@ -0,0 +1,14 @@
+def check_Instance(obj, cls):
+    return check_subclass(type(obj), cls)
+
+
+def check_subclass(child, base):
+    if child == base:
+        return True
+
+    for direct_base in child.__basses__:
+        if base == direct_base:
+            return True
+        return check_subclass(direct_base, base)
+    
+    return False
